502245dc81989af6890971f0bc9156fac5d87bd9
ceelogre <sam.nishimwe@yahoo.com>
2024-06-20 12:44:04 +0200

    feat(WIP): toggle nav menu on mobile

--- trust-fn/src/app/ui/top navbar/nav.tsx
+++ trust-fn/src/app/ui/top navbar/nav.tsx
@@ -1,19 +1,29 @@
+"use client"
+
+import { useState } from 'react'
 import styles from './nav.module.css'
 import Link from 'next/link'
 
 const Nav = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     const navItems = {
         '/#': "Services",
         "/#hiw": "How It Works",
         '/auth': "Join"
+    }
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen)
     }
     return (
         <div className={styles.navigation}>
             <div className={styles.nav_left}>
                 <Link href='/'><h1>Trust</h1></Link>
             </div>
-            <div >
-                <ul className={styles.nav_right}>
+            <div className= {styles.nav_right}>
+                <button className={styles.mobileMenuButton} onClick={toggleMobileMenu}>
+                     ☰
+                </button>
+                <ul className={`${styles.navList} ${mobileMenuOpen? styles.navListMobile : ''}`}>
                     {
                         Object.entries(navItems).map(([key_, value]) => (
                             <Link href={key_}><li key={key_}>{value}</li></Link>
