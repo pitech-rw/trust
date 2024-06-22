8037cbfa2c695a3e17e9216749f9d43f4dab664e
Marie Claudine Ingenere <ingenerem@gmail.com>
2024-06-22 00:27:25 -0400

    Adding backend code for basic database queries and setup

---
+++ trust-bn/src/main/java/pitech/trust/model/User.java
@@ -0,0 +1,28 @@
+package pitech.trust.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String nationalId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String location;
+    private String role;
+    private String status;
+    private String username;
+    private String password;
+
+
+
+
+}
