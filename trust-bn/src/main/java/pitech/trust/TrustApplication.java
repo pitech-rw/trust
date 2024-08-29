71f398dec1d1c7ca6fb2efeaf73198b934e65039
Marie Claudine Ingenere <ingenerem@gmail.com>
2024-08-29 00:58:56 -0400

    adding .env file

--- trust-bn/src/main/java/pitech/trust/TrustApplication.java
+++ trust-bn/src/main/java/pitech/trust/TrustApplication.java
@@ -1,5 +1,6 @@
 package pitech.trust;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrustApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(TrustApplication.class, args);
 	}
 
